6f24cb2b2492f4c11843d8f15dc837b64ab623cc
mimco <klajnsek.mitja@gmail.com>
2025-05-08 09:04:12 +0200

    upgraded controller and router

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,40 @@ var UserModel = require('../models/userModel.js');
 
 module.exports = {
 
+    list: function (req, res) {
+        UserModel.find(function (err, users) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting user.',
+                    error: err
+                });
+            }
+
+            return res.json(users);
+        });
+    },
+
+    show: function (req, res) {
+        var id = req.params.id;
+
+        UserModel.findOne({_id: id}, function (err, user) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting user.',
+                    error: err
+                });
+            }
+
+            if (!user) {
+                return res.status(404).json({
+                    message: 'No such user'
+                });
+            }
+
+            return res.json(user);
+        });
+    },
+
     create: function (req, res) {
         var user = new UserModel({
 			username : req.body.username,
@@ -21,6 +55,40 @@ module.exports = {
         });
     },
 
+    update: function (req, res) {
+        var id = req.params.id;
+
+        UserModel.findOne({_id: id}, function (err, user) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting user',
+                    error: err
+                });
+            }
+
+            if (!user) {
+                return res.status(404).json({
+                    message: 'No such user'
+                });
+            }
+
+            user.username = req.body.username ? req.body.username : user.username;
+			user.password = req.body.password ? req.body.password : user.password;
+			user.email = req.body.email ? req.body.email : user.email;
+			
+            user.save(function (err, user) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when updating user.',
+                        error: err
+                    });
+                }
+
+                return res.json(user);
+            });
+        });
+    },
+
     remove: function (req, res) {
         var id = req.params.id;
 
@@ -36,6 +104,7 @@ module.exports = {
         });
     },
 
+
     login: function(req, res, next){
         UserModel.authenticate(req.body.username, req.body.password, function(err, user){
             if(err || !user){
@@ -46,6 +115,23 @@ module.exports = {
             req.session.userId = user._id;
             return res.json(user);
         });
+    },
+
+    profile: function(req, res,next){
+        UserModel.findById(req.session.userId)
+        .exec(function(error, user){
+            if(error){
+                return next(error);
+            } else{
+                if(user===null){
+                    var err = new Error('Not authorized, go back!');
+                    err.status = 400;
+                    return next(err);
+                } else{
+                    return res.json(user);
+                }
+            }
+        });  
     },
 
     logout: function(req, res, next){
