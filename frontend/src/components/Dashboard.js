7c9b9f78467695b9302000e05c823da92c73aa6d
RokAnzel <roki.anzel@gmail.com>
2025-05-30 16:50:42 +0200

    added basic function for dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,3 +1,14 @@
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../userContext';
 import { Navigate } from 'react-router-dom';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Dashboard</h2>
+      <p>Welcome, Owner! This is your dashboard.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
