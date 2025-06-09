fa596e96216ca7e3a1d2a45f3a30baa0f74da407
janmilosic <milosic.jan@gmail.com>
2025-06-09 20:50:27 +0200

    Testing CI/CD

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders CocktailBox title in navbar', () => {
+test('CocktailBox title in navbar', () => {
   render(<App />);
   const headerElements = screen.getAllByText(/CocktailBox/i);
   expect(headerElements[0]).toBeTruthy();
