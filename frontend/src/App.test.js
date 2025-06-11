19a11819d760e0eea02f4f3108a6931cf5ced283
janmilosic <milosic.jan@gmail.com>
2025-06-11 09:30:37 +0200

    Added 2 more simple tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('CocktailBox title in navbar', () => {
+test('CocktailBox title', () => {
   render(<App />);
   const headerElements = screen.getAllByText(/CocktailBox/i);
   expect(headerElements[0]).toBeTruthy();
+});
+
+test('Login link', () => {
+  render(<App />);
+  const loginLink = screen.getByText(/Prijava/i);
+  expect(loginLink).toBeTruthy();
+});
+
+test('Register link', () => {
+  render(<App />);
+  const registerLink = screen.getByText(/Registracija/i);
+  expect(registerLink).toBeTruthy();
 });
